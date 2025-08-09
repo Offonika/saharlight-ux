ccd04b34cb4e383057285376a836b1ab385c8cd9
Offonika <arseniikeshtov@gmail.com>
2025-08-09 20:58:39 +0300

    [webapp] Add base path for BrowserRouter

--- webapp/spa/src/App.tsx
+++ webapp/spa/src/App.tsx
@@ -45,7 +45,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ui">
         <AppContent />
       </BrowserRouter>
     </TooltipProvider>
