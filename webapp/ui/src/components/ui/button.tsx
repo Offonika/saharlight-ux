c75e903c50cd490fa9dd956e63c4499e954fab73
Offonika <arseniikeshtov@gmail.com>
2025-08-11 09:23:53 +0300

    [ui] Add height to large button

--- webapp/ui/src/components/ui/button.tsx
+++ webapp/ui/src/components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "rounded-xl px-6 py-3",
+        lg: "h-12 rounded-xl px-6 py-3 text-base",
         icon: "h-10 w-10",
       },
     },
