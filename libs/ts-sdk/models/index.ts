8558796e6e82906b3755b8405c1f119f22c4ffef
Offonika <arseniikeshtov@gmail.com>
2025-08-12 13:41:50 +0300

    [sdk] switch to openapi sdk generators

--- libs/ts-sdk/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ApiRemindersGet200Response';
-export * from './ApiRemindersPost200Response';
-export * from './Profile';
-export * from './Reminder';
-export * from './Status';
-export * from './Timezone';
