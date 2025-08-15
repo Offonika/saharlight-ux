72dcf75dd0df8af6f6242026c9b7e16c47a0b7a7
Offonika <arseniikeshtov@gmail.com>
2025-08-15 13:30:19 +0300

    [ui] Remove nested router wrapper

--- services/webapp/ui/src/App.tsx
+++ services/webapp/ui/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useTelegram } from "@/hooks/useTelegram";
 import { ThemeProvider } from "next-themes";
 import { Suspense, lazy } from "react";
@@ -60,9 +60,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/ui">
-          <AppContent />
-        </BrowserRouter>
+        <AppContent />
       </TooltipProvider>
     </QueryClientProvider>
   </ThemeProvider>
