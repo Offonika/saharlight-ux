9d984c53ac50b3560936e9c49d8e8ad8370e5b33
Offonika <arseniikeshtov@gmail.com>
2025-08-15 16:43:13 +0300

    [webapp] guard color methods for old clients

--- services/webapp/ui/public/telegram-init.js
+++ services/webapp/ui/public/telegram-init.js
@@ -4,13 +4,25 @@
         return;
     }
 
-    const applyTheme = () => {
-        app.setBackgroundColor('#fff');
-        app.setHeaderColor('#fff');
+    const supportsColorMethods = () => {
+        const [major = 0, minor = 0] = (app.version || '0.0')
+            .split('.')
+            .map((n) => parseInt(n, 10));
+        if (app.platform === 'tdesktop') {
+            return major > 4 || (major === 4 && minor >= 8);
+        }
+        return major > 6 || (major === 6 && minor >= 1);
     };
 
-    app.expand();
+    const applyTheme = () => {
+        if (supportsColorMethods()) {
+            if (app.setBackgroundColor) app.setBackgroundColor('#fff');
+            if (app.setHeaderColor) app.setHeaderColor('#fff');
+        }
+    };
+
+    app.expand?.();
     applyTheme();
-    app.onEvent('themeChanged', applyTheme);
+    app.onEvent?.('themeChanged', applyTheme);
 })();
 
