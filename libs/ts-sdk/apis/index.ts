8b7571b5e3fa70cde02ca1812a75c1c665b393f4
Offonika <arseniikeshtov@gmail.com>
2025-08-23 07:43:55 -0500

    [sdk] add generated ts-sdk workspace

---
+++ libs/ts-sdk/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './HistoryApi';
+export * from './ProfilesApi';
+export * from './RemindersApi';
---
+++ libs/ts-sdk/models/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnalyticsPoint';
+export * from './DayStats';
+export * from './HTTPValidationError';
+export * from './HistoryRecordSchemaInput';
+export * from './HistoryRecordSchemaOutput';
+export * from './ProfileSchema';
+export * from './ReminderSchema';
+export * from './RoleSchema';
+export * from './Timezone';
+export * from './UserContext';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
+export * from './WebUser';
