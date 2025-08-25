158e4e9b89d0409d5dcd14fccac0a4fe0f5b4eb8
Offonika <arseniikeshtov@gmail.com>
2025-08-25 11:40:46 -0500

    [py-sdk] Refactor multipart form handling

--- libs/py-sdk/test/test_rest_multipart.py
+++ libs/py-sdk/test/test_rest_multipart.py
@@ -18,152 +18,43 @@ def _mock_response() -> urllib3.HTTPResponse:
     return urllib3.HTTPResponse(body=b"", status=200, headers={})
 
 
+def _call_fields(post_params: object) -> list[tuple[str, str]]:
+    client = _client()
+    mock = MagicMock(return_value=_mock_response())
+    client.pool_manager.request = mock  # type: ignore[method-assign]
+    client.request(  # type: ignore[no-untyped-call]
+        "POST",
+        "http://example.com",
+        headers={"Content-Type": "multipart/form-data"},
+        post_params=post_params,
+    )
+    kwargs = mock.call_args.kwargs
+    assert kwargs["encode_multipart"] is True
+    assert all(len(item) == 2 for item in kwargs["fields"])
+    return kwargs["fields"]
+
+
 def test_multipart_dict() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params={"foo": "bar", "baz": {"a": 1}},
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [
-        ("foo", "bar"),
-        ("baz", json.dumps({"a": 1})),
-    ]
-
-
-def test_multipart_list() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params=[("foo", "bar"), ("baz", {"a": 1})],
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [
-        ("foo", "bar"),
-        ("baz", json.dumps({"a": 1})),
-    ]
-
-
-def test_multipart_tuple() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params=(("foo", "bar"), ("baz", {"a": 1})),
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [
-        ("foo", "bar"),
-        ("baz", json.dumps({"a": 1})),
-    ]
-
-
-def test_multipart_generator() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
-    params = ((k, v) for k, v in [("foo", "bar"), ("baz", {"a": 1})])
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params=params,
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [
-        ("foo", "bar"),
-        ("baz", json.dumps({"a": 1})),
-    ]
+    fields = _call_fields({"foo": "bar", "baz": {"a": 1}})
+    assert fields == [("foo", "bar"), ("baz", json.dumps({"a": 1}))]
 
 
 def test_multipart_mapping() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
     params = OrderedDict([("foo", "bar"), ("baz", {"a": 1})])
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params=params,
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [
-        ("foo", "bar"),
-        ("baz", json.dumps({"a": 1})),
-    ]
+    fields = _call_fields(params)
+    assert fields == [("foo", "bar"), ("baz", json.dumps({"a": 1}))]
 
 
 def test_multipart_nested_dict() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
     nested = {"x": {"y": {"z": 2}}}
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params={"nested": nested},
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [("nested", json.dumps(nested))]
+    fields = _call_fields({"nested": nested})
+    assert fields == [("nested", json.dumps(nested))]
 
 
 def test_multipart_list_value() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
     value = [1, 2]
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params={"list": value},
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [("list", json.dumps(value))]
-
-
-def test_multipart_tuple_value() -> None:
-    client = _client()
-    mock = MagicMock(return_value=_mock_response())
-    client.pool_manager.request = mock  # type: ignore[method-assign]
-    value = (1, 2)
-    client.request(  # type: ignore[no-untyped-call]
-        "POST",
-        "http://example.com",
-        headers={"Content-Type": "multipart/form-data"},
-        post_params={"tuple": value},
-    )
-    kwargs = mock.call_args.kwargs
-    assert kwargs["encode_multipart"] is True
-    assert all(len(item) == 2 for item in kwargs["fields"])
-    assert kwargs["fields"] == [("tuple", json.dumps(value))]
+    fields = _call_fields({"list": value})
+    assert fields == [("list", json.dumps(value))]
 
 
 @pytest.mark.parametrize(
@@ -194,3 +85,4 @@ def test_multipart_invalid_string() -> None:
             headers={"Content-Type": "multipart/form-data"},
             post_params="invalid",
         )
+
