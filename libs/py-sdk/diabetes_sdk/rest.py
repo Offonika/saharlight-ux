79b10037e89101224c893e4a1bea717486d40fa3
Offonika <arseniikeshtov@gmail.com>
2025-08-26 13:58:07 -0500

    [py-sdk] Validate REST method

--- libs/py-sdk/diabetes_sdk/rest.py
+++ libs/py-sdk/diabetes_sdk/rest.py
@@ -145,7 +145,8 @@ class RESTClientObject:
                                  (connection, read) timeouts.
         """
         method = method.upper()
-        assert method in ["GET", "HEAD", "DELETE", "POST", "PUT", "PATCH", "OPTIONS"]
+        if method not in ["GET", "HEAD", "DELETE", "POST", "PUT", "PATCH", "OPTIONS"]:
+            raise ApiValueError(f"{method} is not a supported HTTP method")
 
         if post_params and body:
             raise ApiValueError(
