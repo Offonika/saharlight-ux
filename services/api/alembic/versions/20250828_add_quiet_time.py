35fa464b84c71eedb0f3c388458ce57232e42f8d
Offonika <arseniikeshtov@gmail.com>
2025-09-04 13:22:19 -0500

    [migrations] rename quiet time revision

--- services/api/alembic/versions/20250828_add_quiet_time.py
+++ services/api/alembic/versions/20250828_add_quiet_time.py
@@ -5,7 +5,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "20250828_add_quiet_time_to_profiles"
+revision: str = "20250828_add_quiet_time"
 down_revision: Union[str, None] = "20250825_add_quiet_hours_to_profiles"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
