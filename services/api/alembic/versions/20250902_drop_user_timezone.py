8fc2f98e387c7fe8530b074dd0169b3d1f5a8ac1
Offonika <arseniikeshtov@gmail.com>
2025-09-04 13:03:20 -0500

    [alembic] Fix down revision for dropping user timezone

--- services/api/alembic/versions/20250902_drop_user_timezone.py
+++ services/api/alembic/versions/20250902_drop_user_timezone.py
@@ -6,7 +6,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = "20250902_drop_user_timezone"
-down_revision: Union[str, Sequence[str], None] = "20250904_change_description"
+down_revision: Union[str, Sequence[str], None] = "20250902_reminder_log_telegram_foreign_key"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
