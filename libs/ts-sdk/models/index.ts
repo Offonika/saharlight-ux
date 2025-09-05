3ed48dff9cafdc08bc9eed16a1c94ec9629f76f8
Offonika <arseniikeshtov@gmail.com>
2025-09-05 14:06:52 -0500

    contracts: add onboarding api

--- libs/ts-sdk/models/index.ts
+++ libs/ts-sdk/models/index.ts
@@ -2,14 +2,17 @@
 /* eslint-disable */
 export * from './AnalyticsPoint';
 export * from './DayStats';
+export * from './EventPayload';
 export * from './HTTPValidationError';
 export * from './HistoryRecordSchemaInput';
 export * from './HistoryRecordSchemaOutput';
+export * from './OnboardingStatusResponse';
 export * from './ProfileSchema';
 export * from './ProfileSettingsIn';
 export * from './ProfileSettingsOut';
 export * from './ReminderSchema';
 export * from './ReminderType';
+export * from './ResponseOnboardingEventsPost';
 export * from './RoleSchema';
 export * from './ScheduleKind';
 export * from './Timezone';
