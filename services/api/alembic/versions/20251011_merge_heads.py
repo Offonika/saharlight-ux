45e4bc732420c8d2ee5af3dd7c368865684009a5
Offonika <arseniikeshtov@gmail.com>
2025-09-07 13:01:01 -0500

    [alembic] Rename merge heads revision file

--- services/api/alembic/versions/20251011_merge_heads.py
+++ services/api/alembic/versions/20251011_merge_heads.py
@@ -1,10 +1,11 @@
 """merge heads
 
 Revision ID: 20251011_merge_heads
-Revises: 11eb7c3deda6, 20251010_lesson_logs_user_plan_index
+Revises: 20250904_merge_heads, 20251010_lesson_logs_user_plan_index
 Create Date: 2025-09-07 15:17:50.994294
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,11 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '20251011_merge_heads'
-down_revision: Union[str, None] = ('11eb7c3deda6', '20251010_lesson_logs_user_plan_index')
+revision: str = "20251011_merge_heads"
+down_revision: Union[str, None] = (
+    "20250904_merge_heads",
+    "20251010_lesson_logs_user_plan_index",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
