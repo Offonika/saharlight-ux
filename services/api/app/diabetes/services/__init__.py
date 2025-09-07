d5c10e7da6dfc835a24ab3a3d7c6dd4a9c315612
Offonika <arseniikeshtov@gmail.com>
2025-09-07 10:16:43 -0500

    [services] expose lesson log repository

--- services/api/app/diabetes/services/__init__.py
+++ services/api/app/diabetes/services/__init__.py
@@ -1,3 +1,4 @@
+from services.api.app.assistant.repositories import logs as lesson_log
 from .user_profile import save_timezone
 
-__all__ = ["save_timezone"]
+__all__ = ["save_timezone", "lesson_log"]
