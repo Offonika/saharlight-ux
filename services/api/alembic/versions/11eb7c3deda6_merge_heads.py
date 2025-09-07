9d1a7ea00c332e90477f36badd32c1f7b38f4d8f
Offonika <arseniikeshtov@gmail.com>
2025-09-07 15:00:46 -0500

    [alembic] Fix merge heads revision references

--- services/api/alembic/versions/11eb7c3deda6_merge_heads.py
+++ services/api/alembic/versions/11eb7c3deda6_merge_heads.py
@@ -1,6 +1,6 @@
 """merge heads
 
-Revision ID: 11eb7c3deda6
+Revision ID: 11eb7c3deda6_merge_heads
 Revises: 20251002_billing_event_lowercase, 20251002_subscription_plan_values_callable
 Create Date: 2025-09-04 19:44:43.266143
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '11eb7c3deda6'
+revision: str = '11eb7c3deda6_merge_heads'
 down_revision: Union[str, None] = ('20251002_billing_event_lowercase', '20251002_subscription_plan_values_callable')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- services/api/alembic/versions/11eb7c3deda6_merge_onboarding_billing.py
+++ services/api/alembic/versions/11eb7c3deda6_merge_onboarding_billing.py
@@ -1,7 +1,7 @@
 """merge onboarding billing
 
 Revision ID: 11eb7c3deda6_merge_onboarding_billing
-Revises: 11eb7c3deda6, 20251003_onboarding_event
+Revises: 11eb7c3deda6_merge_heads, 20251003_onboarding_event
 Create Date: 2025-10-03 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ from typing import Sequence, Union
 # revision identifiers, used by Alembic.
 revision: str = "11eb7c3deda6_merge_onboarding_billing"
 down_revision: Union[str, Sequence[str], None] = (
-    "11eb7c3deda6",
+    "11eb7c3deda6_merge_heads",
     "20251003_onboarding_event",
 )
 branch_labels: Union[str, Sequence[str], None] = None
